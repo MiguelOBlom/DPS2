a6d1df1bde4fb62fba9a209f0cf1a8428fc0ec1b
Miguel Blom <migueloblom@gmail.com>
2021-12-16 03:15:37 +0100

    Update

--- stop_das5.sh
+++ stop_das5.sh
@@ -1,3 +1,7 @@
 # Miguel Blom
 echo "Cancelling previous reservations..."
-preserve -c $(preserve -llist | tail -n+4 | grep $(whoami) | sort -r | head -n1 | awk '{print $1}')
+RES_ID=$(preserve -llist | tail -n+4 | grep $(whoami) | sort -r | head -n1 | awk '{print $1}')
+if [ -n "$RES_ID" ]
+then
+	preserve -c $RES_ID
+fi
