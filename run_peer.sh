3984eff46e1fa55865a7ba4f991b930d909676f0
Miguel Blom <migueloblom@gmail.com>
2021-12-16 05:08:09 +0100

    deployment with log

--- run_peer.sh
+++ run_peer.sh
@@ -7,9 +7,9 @@ source $DPS2_DIR/netinfo.txt
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH":$DPS2_DIR/application/blockchain:$DPS2_DIR/application/p2p:$DPS2_DIR/application/pow:$DPS2_DIR/application/sha256:$DPS2_DIR/application/sqlite3
 
 IP=$(ifconfig | grep inet | grep -o '10\.149\.\S*' | awk -F . '$NF !~ /^255/')
-
+echo $IP>>out.txt
 mkdir -p $DPS2_DIR/$EXPERIMENT_NAME/out
 
-echo $DPS2_DIR/application/build/application $TRACKER_IP $TRACKER_PORT $IP $PEER_PORT $DPS2_DIR/$EXPERIMENT_NAME/$IP_$PEER_PORT.trc $DPS2_DIR/$EXPERIMENT_NAME/out/$IP_$PEER_PORT.out >> out.txt
-$DPS2_DIR/application/build/application $TRACKER_IP $TRACKER_PORT $IP $PEER_PORT $DPS2_DIR/$EXPERIMENT_NAME/$IP_$PEER_PORT.trc $DPS2_DIR/$EXPERIMENT_NAME/out/$IP_$PEER_PORT.out
+echo $DPS2_DIR/application/build/application $TRACKER_IP $TRACKER_PORT $IP $PEER_PORT $DPS2_DIR/$EXPERIMENT_NAME/${IP}_${PEER_PORT}.trc $DPS2_DIR/$EXPERIMENT_NAME/out/${IP}_${PEER_PORT}.out >> out.txt
+$DPS2_DIR/application/build/application $TRACKER_IP $TRACKER_PORT $IP $PEER_PORT $DPS2_DIR/$EXPERIMENT_NAME/${IP}_${PEER_PORT}.trc $DPS2_DIR/$EXPERIMENT_NAME/out/${IP}_${PEER_PORT}.out
 sleep infinity
